4f1ed8a9f7b97fb89ad26cee96ea65e29b84f315
Name <email@example.ru>
2021-10-12 22:12:52 +1100

    Добавьте файлы проекта.

---
+++ Telegram_bot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Telegram_bot
+{
+    /// <summary>
+    /// Base class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Base method
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
